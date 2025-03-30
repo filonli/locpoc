18daefb7be82d40e99681ec74e553346b7fe82e3
filonli <squirrelreal@gmail.com>
2025-03-30 15:04:51 +0300

    Update app.py

--- app.py
+++ app.py
@@ -49,13 +49,13 @@ messages = st.container()
 
 for row in rows.data:
     
-    messages.chat_message(row["sender"]+":").write(row["sender"]+": "+row["message"])
+    messages.chat_message(row["sender"]+":").badge(row["sender"]+": ").write(row["message"])
 
 
 if prompt := st.chat_input("Say something"):
     d = {"sender":name,"message":prompt}
     supabase.table("messages").insert(d).execute()
-    messages.chat_message('human').write(name+": "+prompt)
+    messages.chat_message('human').badge(name).write(prompt)
 
     
 
