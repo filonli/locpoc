2caa436097cf1e4203e147bcb09dffbe90dcacb4
filonli <squirrelreal@gmail.com>
2025-03-30 13:46:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ name = st.text_input("write your name")
 messages = st.container(height=300)
 if prompt := st.chat_input("Say something"):
     d = {"sender":name,"message":prompt}
-    response = supabase.table["messages"].insert([d]).execute()
+    supabase.table["messages"].insert([d]).execute()
 
 
 for row in rows.data:
