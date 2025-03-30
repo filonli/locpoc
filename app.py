42a2cfbdb8bda63f9e1fca97017d710a75409843
filonli <squirrelreal@gmail.com>
2025-03-30 13:33:39 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,13 @@ def init_connection():
     key = st.secrets["SUP_KEY"]
     return create_client(url, key)
 
-supabase = init_connection()
+base = init_connection()
 
 # Perform query.
 # Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
 def run_query():
-    return supabase.table("messages").select("*").execute()
+    return base.table("messages").select("*").execute()
 
 rows = run_query()
 
@@ -33,7 +33,7 @@ name = st.text_input("write your name")
 messages = st.container(height=300)
 if prompt := st.chat_input("Say something"):
     d = {"sender":name,"message":prompt}
-    response = supabase.table["messages"].insert(d).execute()
+    response = base.table["messages"].insert(d).execute()
 
 
 for row in rows.data:
