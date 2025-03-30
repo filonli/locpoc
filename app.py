3ac8bc55a78a7498be090b7915b3570a72e3acb3
filonli <squirrelreal@gmail.com>
2025-03-30 12:03:03 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import array
+
+st.set_page_config(page_title="locpoc")
+
+st.title("locpoc")
+st.markdown("annonymous public messanger")
+
+sta = st.session_state
+
+if 'chat' not in st.session_state:
+    sta.chat = []
+
+name = st.text_input("write your name")
+
+messages = st.container(height=300)
+if prompt := st.chat_input("Say something"):
+    msg = {}
+    msg["name"] = name
+    msg["text"] = prompt
+    sta.chat.append(msg)
+
+for m in sta.chat:
+    
+    messages.chat_message(m["name"]+":").write(m["name"]+": "+m["text"])
